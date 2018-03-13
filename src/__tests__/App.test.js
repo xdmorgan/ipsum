7703069fa01301d86c3a3f9a075a07d35e065a50
Danny Morgan <dan@dannydev.com>
2018-03-13 18:50:21 -0400

    [#6] Add GeneratedText component to main App view

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -3,6 +3,9 @@ import renderer from "react-test-renderer";
 import App from "../App";
 import "jest-styled-components";
 
+import * as generator from "../generator";
+generator.generate = () => "Randomly generated String";
+
 it("renders without crashing", () => {
   const tree = renderer.create(<App />);
   expect(tree).toMatchSnapshot();
