84ea8371f82cbad38c7a14eea0fd768c5ed3488c
Danny Morgan <dan@dannydev.com>
2018-03-13 18:49:21 -0400

    [#6] Add Flow types to util functions and add text renderer component

--- src/generator/index.js
+++ src/generator/index.js
@@ -1,29 +1,27 @@
-const MOCK_DATA = [
-  "Rafas mags",
-  "Shola on a bike eating an apple",
-  "Mandy's mags",
-  "Rafa putting his glasses in his pocket, job done lads!",
-  "Pardew headbutting David Meyler",
-  "Yohan Kebabs",
-  "Ben Arfa's goal against Everton",
-  "Ben Arfa's goal against Blackburn",
-  "Geordie boys"
-];
+// @flow
 
 /**
  * Return a random entry from the supplied collection. Use js default
  * Math.random() unless another randomizer function is provided.
- * @param {any[]} arr An arbitrary collection of whatever
- * @param {() => number} rand A randomizer function (returns 0-1)
+ * @param {*} arr An arbitrary collection of whatever
+ * @param {*} rand A randomizer function (returns 0-1)
  */
-const randomEntryInArray = (arr = [], rand = Math.random) =>
-  arr[Math.floor(rand() * (arr.length - 1))];
+const randomEntryInArray = (
+  arr: any[] = [],
+  rand: () => number = Math.random
+) => arr[Math.floor(rand() * (arr.length - 1))];
 
-const randomCollection = (dict = [], length = 100) =>
+/**
+ *
+ * @param {*} dict A dictionary of strings to select from
+ * @param {*} length How many entries to randomly select
+ */
+const randomCollection = (dict: string[] = [], length: number = 100) =>
   Array.from({ length }).map(() => randomEntryInArray(dict));
 
-const generate = (data = MOCK_DATA, len) =>
-  randomCollection(data, len).join(". ");
+const generate = (data: string[], length: number) =>
+  randomCollection(data, length).join(". ");
 
+export { default as GeneratedText } from "./GeneratedText";
 export { randomEntryInArray, randomCollection, generate };
 export default generate;
