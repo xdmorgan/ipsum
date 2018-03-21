4236254aa0ebadd2404557d957e6c544ac80d5d8
Danny Morgan <dan@dannydev.com>
2018-03-21 19:10:57 -0400

    Start global layout component(s)

--- src/App.js
+++ src/App.js
@@ -2,30 +2,13 @@
 
 import React, { Component } from "react";
 import styled from "styled-components/primitives";
-import logo from "./logo.svg";
 import { NUFC } from "./generator/data";
 import { GeneratedText } from "./generator";
-
-const AppView = styled.View`
-  display: flex;
-  align-items: center;
-`;
-const HeaderView = styled.View`
-  background-color: #222;
-  height: 150px;
-  padding: 20px;
-  align-self: stretch;
-  align-items: center;
-`;
-
-const LogoImage = styled.Image`
-  width: 80px;
-  height: 80px;
-`;
+import { AppLayout, ContainerView } from "./global";
 
 const IntroHeading = styled.Text`
-  font-size: 1.5em;
-  color: #fff;
+  font-weight: 700;
+  font-size: 32px;
 `;
 
 type State = {
@@ -43,17 +26,12 @@ class App extends Component<{}, State> {
   }
   render() {
     return (
-      <AppView>
-        <HeaderView>
-          <LogoImage
-            source={logo}
-            style={{ resizeMode: "contain" }}
-            alt="logo"
-          />
-          <IntroHeading>Welcome to React</IntroHeading>
-        </HeaderView>
-        <GeneratedText data={this.state.data} length={this.state.length} />
-      </AppView>
+      <AppLayout>
+        <ContainerView>
+          <IntroHeading>Lorem Ipsum Generator</IntroHeading>
+          <GeneratedText data={this.state.data} length={this.state.length} />
+        </ContainerView>
+      </AppLayout>
     );
   }
 }
--- src/generator/GeneratedText.js
+++ src/generator/GeneratedText.js
@@ -3,10 +3,12 @@ import styled from "styled-components/primitives";
 import { generate } from "./";
 
 const StyledTextView = styled.View`
-  padding: 20px;
+  padding: 20px 0;
 `;
 const StyledText = styled.Text`
   color: #111;
+  font-size: 18px
+  line-height: ${18 * 1.4}px;
 `;
 
 export default class GeneratedText extends React.Component {
